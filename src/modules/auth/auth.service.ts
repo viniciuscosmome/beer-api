63bfb2933c12af727297b43ed0d66bb61e73de07
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-10-08 05:55:02 -0300

    feat: process new account data

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,7 +1,24 @@
+import { hash } from 'bcryptjs';
 import { UnauthorizedException } from '../../globals/exceptions';
+import { authRepository } from './auth.repository';
 
 export const authService = {
-  async validatesAccessData(input: iSignInInput): Promise<iValidatesAccessDataOutput> {
+  async processNewAccountData(input: iSignUpInput): Promise<void> {
+    const { password } = input;
+
+    const hashedPassword = await hash(password, 10);
+
+    await authRepository.savesNewAccountData({
+      ...input,
+      password: hashedPassword,
+    });
+
+    return;
+  },
+
+  async validatesAccessData(
+    input: iSignInInput,
+  ): Promise<iValidatesAccessDataOutput> {
     const { email, password } = input;
 
     if (email !== 'email@example.com') {
