6ccda153302d41901149d5b02224e01fa7c98bb9
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-10-08 05:56:36 -0300

    chore: creates sign up controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,18 +1,21 @@
 import { Request, Response } from 'express';
-import { SignInEntity } from './auth.entity';
+import { SignUpEntity, SignInEntity } from './auth.entity';
 import { authService } from './auth.service';
 
 export const authController = {
+  async signUp(req: Request, res: Response) {
+    const input = SignUpEntity.parse(req.body);
+
+    await authService.processNewAccountData(input);
+
+    return res.status(201).json({ msg: 'OK' });
+  },
+
   async signIn(req: Request, res: Response) {
     const input: iSignInInput = SignInEntity.parse(req.body);
 
-    const user = await authService.validatesAccessData(input);
+    console.log('Sign in', input);
 
-    return res.status(200).json({
-      message: 'Conectado!',
-      token: 'A123456789B',
-      refreshToken: 'C987654321D',
-      user: user,
-    });
+    return res.status(200).json({ msg: 'OK' });
   },
 };
