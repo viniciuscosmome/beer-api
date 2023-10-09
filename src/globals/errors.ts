9d3cd4b9b911849bbef4fcc87ab07254df55d4ef
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-10-09 04:26:33 -0300

    chore: handle jwt errors

--- src/globals/errors.ts
+++ src/globals/errors.ts
@@ -4,19 +4,45 @@ import {
   InternalServerErrorException,
   UniqueConstraintFailedException,
 } from './exceptions';
+import {
+  JsonWebTokenError,
+  NotBeforeError,
+  TokenExpiredError,
+} from 'jsonwebtoken';
+import {
+  InvalidTokenException,
+  TokenExpiredException,
+  TokenIsNotActivatedException,
+} from './exceptions';
 
-function handlePrismaErrors(error: Prisma.PrismaClientKnownRequestError) {
+const handlePrismaErrors = (error: Prisma.PrismaClientKnownRequestError) => {
   const target = (error.meta?.target as Array<string>) || ['unknow_meta'];
 
   switch (error.code) {
     case 'P2002':
       throw new UniqueConstraintFailedException(target[0]);
   }
-}
+};
 
-export function handleErrors(error: unknown) {
+const handleJWTErrors = (error: JsonWebTokenError) => {
+  if (error instanceof TokenExpiredError) {
+    throw new TokenExpiredException();
+  }
+
+  if (error instanceof NotBeforeError) {
+    throw new TokenIsNotActivatedException();
+  }
+
+  throw new InvalidTokenException();
+};
+
+export const handleErrors = (error: unknown) => {
   if (error instanceof Prisma.PrismaClientKnownRequestError) {
     handlePrismaErrors(error);
+  }
+
+  if (error instanceof JsonWebTokenError) {
+    handleJWTErrors(error);
   }
 
   if (!IS_PRODUCTION_ENV) {
@@ -24,4 +50,4 @@ export function handleErrors(error: unknown) {
   }
 
   throw new InternalServerErrorException();
-}
+};
