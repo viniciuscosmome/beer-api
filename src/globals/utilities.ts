655171b089b136d032264573c372d5b0e82b7f38
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-10-08 23:51:59 -0300

    feat: generate access token and refresh token

--- src/globals/utilities.ts
+++ src/globals/utilities.ts
@@ -1,3 +1,27 @@
+import { randomUUID } from 'node:crypto';
+import jwt from 'jsonwebtoken';
+import { SECRET_SESS } from './constants';
+
 export const ageLimit = (age: number) => {
   return new Date(new Date().getFullYear() - age, 11, 31);
+};
+
+export const genAccessToken = (payload: object): string => {
+  const token = jwt.sign(payload, SECRET_SESS, {
+    expiresIn: '1h',
+    subject: 'access',
+  });
+
+  return token;
+};
+
+export const genRefreshToken = (payload: object): string => {
+  const jwtid = randomUUID();
+  const token = jwt.sign(payload, SECRET_SESS, {
+    expiresIn: '3h',
+    jwtid: jwtid,
+    subject: 'refresh',
+  });
+
+  return token;
 };
