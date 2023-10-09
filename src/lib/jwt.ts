1593a6ec6c20afc35df51f3fd645929f8a75344e
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-10-09 04:27:23 -0300

    feat: check token

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -1,11 +1,36 @@
 import { randomUUID } from 'node:crypto';
 import jwt from 'jsonwebtoken';
 import { SECRET_SESS } from '../globals/constants';
+import { handleErrors } from '../globals/errors';
+
+export const checkToken = <Excpected = object>(
+  token: string,
+  subject: iTokensSubject,
+): Excpected => {
+  const callback = (error: unknown, decoded: unknown) => {
+    if (error) {
+      handleErrors(error);
+    }
+
+    return decoded;
+  };
+
+  const result = jwt.verify(
+    token,
+    SECRET_SESS,
+    {
+      subject,
+    },
+    callback,
+  ) as Excpected;
+
+  return result;
+};
 
 export const genAccessToken = (payload: object): string => {
   const token = jwt.sign(payload, SECRET_SESS, {
     expiresIn: '1h',
-    subject: 'access',
+    subject: 'ACCESS',
   });
 
   return token;
@@ -16,7 +41,7 @@ export const genRefreshToken = (payload: object): string => {
   const token = jwt.sign(payload, SECRET_SESS, {
     expiresIn: '3h',
     jwtid: jwtid,
-    subject: 'refresh',
+    subject: 'REFRESH',
   });
 
   return token;
