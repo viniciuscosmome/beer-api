d23ea9e277ec01167d9f70d9ba1ce8d5f366cf94
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-10-09 11:48:27 -0300

    docs: creates sign-in documentation

--- src/globals/docs/auth.docs.ts
+++ src/globals/docs/auth.docs.ts
@@ -60,6 +60,67 @@ export const authPaths = {
       },
     },
   },
+  '/auth/sign-in': {
+    post: {
+      tags: ['Authentication'],
+      requestBody: {
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/components/schemas/auth:sign-in:input',
+            },
+          },
+        },
+      },
+      responses: {
+        '200': {
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/auth:sign-in:output',
+              },
+            },
+          },
+        },
+        '400': {
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/$400',
+              },
+            },
+          },
+        },
+        '401': {
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/$401',
+              },
+            },
+          },
+        },
+        '422': {
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/$422',
+              },
+            },
+          },
+        },
+        '500': {
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/$500',
+              },
+            },
+          },
+        },
+      },
+    },
+  },
 };
 
 export const authSchemas = {
@@ -94,6 +155,38 @@ export const authSchemas = {
       message: {
         type: 'string',
         example: 'Conta criada com sucesso!',
+      },
+    },
+  },
+  'auth:sign-in:input': {
+    type: 'object',
+    properties: {
+      email: {
+        type: 'string',
+        example: 'email@example.com',
+      },
+      password: {
+        type: 'string',
+        example: 'strongW#3',
+      },
+      remember: {
+        type: 'boolean',
+        example: false,
+      },
+    },
+  },
+  'auth:sign-in:output': {
+    type: 'object',
+    properties: {
+      message: {
+        type: 'string',
+        example: 'Acesso permitido',
+      },
+      accessToken: {
+        type: 'string',
+      },
+      refreshToken: {
+        type: 'string',
       },
     },
   },
