3d7e850129202bfd3acd7217434caea5460d4377
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-10-12 19:51:10 -0300

    chore: use custom responses

--- src/modules/auth/auth.entity.ts
+++ src/modules/auth/auth.entity.ts
@@ -1,26 +1,27 @@
 import { PASSWORD_REGEX } from '../../globals/constants';
+import { responses } from '../../globals/responses';
 import { ageLimit } from '../../globals/utilities';
 import { z } from 'zod';
 
-export const SignUpEntity = z
-  .object({
-    email: z.string().email(),
-    password: z.string().regex(PASSWORD_REGEX),
-    firstName: z.string(),
-    lastName: z.string(),
-    birthdate: z.coerce.date().max(ageLimit(18)),
-  })
-  .required();
+export const SignUpEntity = z.object({
+  email: z.string(responses.datatype.string).email(),
+  password: z.string(responses.datatype.string).regex(PASSWORD_REGEX),
+  firstName: z.string(responses.datatype.string),
+  lastName: z.string(responses.datatype.string),
+  birthdate: z.coerce
+    .date(responses.datatype.date)
+    .max(ageLimit(18), responses.conditions.minAge(18)),
+});
 
 export const SignInEntity = SignUpEntity.pick({
   email: true,
   password: true,
-}).required();
+});
 
 export const ForgotPasswordEntity = SignUpEntity.pick({
   email: true,
-}).required();
+});
 
 export const SetPasswordEntity = SignUpEntity.pick({
   password: true,
-}).required();
+});
