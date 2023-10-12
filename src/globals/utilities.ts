21752488f026795bdcf5f9a670e566213545aabe
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-10-12 08:58:33 -0300

    feat: check if any id is out of limit

--- src/globals/utilities.ts
+++ src/globals/utilities.ts
@@ -4,8 +4,16 @@ export const ageLimit = (age: number) => {
 
 export const limitSearchID = (param: string) => {
   if (param === 'random') return true;
-
   const id = Number(param);
 
   return id > 0 && id <= 325;
+};
+
+export const checkIfAnyIDIsOutOfLimit = (param: string) => {
+  if (!param) return false;
+  const IDsOutOfLimit = param.split('|').filter((id) => {
+    return !limitSearchID(id);
+  });
+
+  return !IDsOutOfLimit.length;
 };
