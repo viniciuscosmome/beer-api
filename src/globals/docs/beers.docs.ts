e862ebcc3dd71f8c063c30e080af571d466de1a6
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-10-12 11:29:20 -0300

    chore: updates beers documentation

--- src/globals/docs/beers.docs.ts
+++ src/globals/docs/beers.docs.ts
@@ -1,4 +1,141 @@
+const queryParam = (name: string, datatype: string, description: string) => ({
+  in: 'query',
+  name: name,
+  required: false,
+  schema: {
+    type: datatype,
+  },
+  description: description,
+});
+
 export const beersPaths = {
+  '/beers': {
+    get: {
+      tags: ['Beers'],
+      summary: 'Busca cerveja usando filtros. Retorna no máximo 50 por página.',
+      security: [
+        {
+          BearerAccess: [],
+        },
+      ],
+      parameters: [
+        queryParam('page', 'number', '> Definir a págia de busca.'),
+        queryParam(
+          'per_page',
+          'number',
+          '> Quantidade de itens por página. `Máximo 80`.',
+        ),
+        queryParam(
+          'abv_gt',
+          'number',
+          '> Retorna todas as cervejas com o ABV maior do que o fornecido.',
+        ),
+        queryParam(
+          'abv_lt',
+          'number',
+          '> Retorna todas as cervejas com o ABV menor do que o fornecido',
+        ),
+        queryParam(
+          'ibu_gt',
+          'number',
+          '> Retorna todas as cervejas com o IBU maior do que o fornecido',
+        ),
+        queryParam(
+          'ibu_lt',
+          'number',
+          '> Retorna todas as cervejas com o IBU menor do que o fornecido',
+        ),
+        queryParam(
+          'ebc_gt',
+          'number',
+          '> Retorna todas as cervejas com o EBC maior do que o fornecido',
+        ),
+        queryParam(
+          'ebc_lt',
+          'number',
+          '> Retorna todas as cervejas com o EBC menor do que o fornecido',
+        ),
+        queryParam(
+          'beer_name',
+          'string',
+          '> Retorna todas as cervejas que correspondem ao nome fornecido, isso inclui strings parciais. Use sublinhado (_) ao invés de espaço em branco.',
+        ),
+        queryParam(
+          'yeast',
+          'string',
+          '> Retorna todas as cervejas que correspondem ao nome de levedura fornecido. Use sublinhado (_) ao invés de espaço em branco.',
+        ),
+        queryParam(
+          'brewed_before',
+          'string',
+          '> Retorna todas as cervejas que tenham a data de fabricação anteriores a fornecida. Use o formato `mm-yyyy`. Ex: 10-2010',
+        ),
+        queryParam(
+          'brewed_after',
+          'string',
+          '> Retorna todas as cervejas que tenham a data de fabricação posteriores a fornecida. Use o formato `mm-yyyy`. Ex: 10-2010',
+        ),
+        queryParam(
+          'hops',
+          'string',
+          '> Retorna todas as cervejas que correspondem ao nome de lúpulo fornecido. Use sublinhado (_) ao invés de espaço em branco.',
+        ),
+        queryParam(
+          'malt',
+          'string',
+          '> Retorna todas as cervejas que correspondem ao nome de malte fornecido. Use sublinhado (_) ao invés de espaço em branco.',
+        ),
+        queryParam(
+          'food',
+          'string',
+          '> Retorna todas as cervejas que combinem com o nome de comida fornecido. Use sublinhado (_) ao invés de espaço em branco.',
+        ),
+        queryParam(
+          'ids',
+          'string',
+          '> Retorna todas as cervejas que contenham um id presente na lista fornecida. Use IDs separador pela barra vetical ( `|` ). Ex: 3|21|35.',
+        ),
+      ],
+      responses: {
+        '200': {
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/beers:output',
+              },
+            },
+          },
+        },
+        '400': {
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/$400',
+              },
+            },
+          },
+        },
+        '401': {
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/$401',
+              },
+            },
+          },
+        },
+        '500': {
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/$500',
+              },
+            },
+          },
+        },
+      },
+    },
+  },
   '/beers/{filter}': {
     get: {
       tags: ['Beers'],
