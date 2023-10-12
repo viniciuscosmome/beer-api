e549d0f43466d220582c156a2b0c1b5c4f4ca90e
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-10-12 09:20:54 -0300

    feat: creates advanced search entity

--- src/modules/beers/beers.entity.ts
+++ src/modules/beers/beers.entity.ts
@@ -1,9 +1,56 @@
 import { z } from 'zod';
-import { BASIC_SEARCH_REGEX } from '../../globals/constants';
-import { limitSearchID } from '../../globals/utilities';
+import {
+  BASIC_SEARCH_REGEX,
+  BEERS_BREWED_REGEX,
+  BEERS_IDS_REGEX,
+  TEXT_SEARCH_REGEX,
+} from '../../globals/constants';
+import {
+  checkBrewedDateFormat,
+  checkIfAnyIDIsOutOfLimit,
+  limitSearchID,
+} from '../../globals/utilities';
+
+const IDLimitError = {
+  message: 'The ID must be between 1 and 325',
+};
 
 export const BasicSearchEntity = z.object({
-  filter: z.string().regex(BASIC_SEARCH_REGEX).refine(limitSearchID, {
-    message: 'The ID must be between 1 and 325',
-  }),
+  filter: z
+    .string()
+    .regex(BASIC_SEARCH_REGEX)
+    .refine(limitSearchID, IDLimitError),
+});
+
+export const AdvancedSearchEntity = z.object({
+  page: z.coerce.number().optional(),
+  abv_gt: z.coerce.number().optional(),
+  abv_lt: z.coerce.number().optional(),
+  ibu_gt: z.coerce.number().optional(),
+  ibu_lt: z.coerce.number().optional(),
+  ebc_gt: z.coerce.number().optional(),
+  ebc_lt: z.coerce.number().optional(),
+  beer_name: z.string().trim().regex(TEXT_SEARCH_REGEX).optional(),
+  yeast: z.string().trim().regex(TEXT_SEARCH_REGEX).optional(),
+  brewed_before: z
+    .string()
+    .trim()
+    .regex(BEERS_BREWED_REGEX)
+    .refine(checkBrewedDateFormat)
+    .optional(),
+  brewed_after: z
+    .string()
+    .trim()
+    .regex(BEERS_BREWED_REGEX)
+    .refine(checkBrewedDateFormat)
+    .optional(),
+  hops: z.string().trim().regex(TEXT_SEARCH_REGEX).optional(),
+  malt: z.string().trim().regex(TEXT_SEARCH_REGEX).optional(),
+  food: z.string().trim().regex(TEXT_SEARCH_REGEX).optional(),
+  ids: z
+    .string()
+    .trim()
+    .regex(BEERS_IDS_REGEX)
+    .refine(checkIfAnyIDIsOutOfLimit, IDLimitError)
+    .optional(),
 });
