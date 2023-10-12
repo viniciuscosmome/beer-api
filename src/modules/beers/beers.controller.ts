423cdc6cec8ffae63c81f2e7d274db7c59d2029b
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-10-12 09:22:04 -0300

    chore: find many beers

--- src/modules/beers/beers.controller.ts
+++ src/modules/beers/beers.controller.ts
@@ -1,13 +1,19 @@
 import { Request, Response } from 'express';
-import { BasicSearchEntity } from './beers.entity';
+import { AdvancedSearchEntity, BasicSearchEntity } from './beers.entity';
 import { beersService } from './beers.service';
 
 export const beersController = {
+  async findManyBeers(req: Request, res: Response) {
+    const filters = AdvancedSearchEntity.parse(req.query);
+    const beers = await beersService.findBeersWithAdvancedFilters(filters);
+
+    return res.status(200).json({ length: beers.length, beers });
+  },
+
   async singleBeer(req: Request, res: Response) {
     const { filter } = BasicSearchEntity.parse(req.params);
-
     const beers = await beersService.findBeersWithBasicFilters(filter);
 
-    return res.status(200).json({ beers });
+    return res.status(200).json({ length: beers.length, beers });
   },
 };
