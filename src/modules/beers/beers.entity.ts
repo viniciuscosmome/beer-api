f482e689df813c52e3a94d6071198afca5b1b03a
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-10-12 19:37:48 -0300

    chore: use custom responses

--- src/modules/beers/beers.entity.ts
+++ src/modules/beers/beers.entity.ts
@@ -6,52 +6,78 @@ import {
   TEXT_SEARCH_REGEX,
 } from '../../globals/constants';
 import {
-  checkBrewedDateFormat,
+  checkBrewedDateRange,
   checkIfAnyIDIsOutOfLimit,
   limitSearchID,
 } from '../../globals/utilities';
-
-const IDLimitError = {
-  message: 'The ID must be between 1 and 325',
-};
+import { responses } from '../../globals/responses';
 
 export const BasicSearchEntity = z.object({
   filter: z
-    .string()
-    .regex(BASIC_SEARCH_REGEX)
-    .refine(limitSearchID, IDLimitError),
+    .string(responses.datatype.string)
+    .regex(BASIC_SEARCH_REGEX, responses.custom.basicSearch)
+    .refine(limitSearchID, responses.custom.idLimit),
 });
 
 export const AdvancedSearchEntity = z.object({
-  page: z.coerce.number().int().min(1).optional(),
-  per_page: z.coerce.number().int().min(1).max(80).optional(),
-  abv_gt: z.coerce.number().optional(),
-  abv_lt: z.coerce.number().optional(),
-  ibu_gt: z.coerce.number().optional(),
-  ibu_lt: z.coerce.number().optional(),
-  ebc_gt: z.coerce.number().optional(),
-  ebc_lt: z.coerce.number().optional(),
-  beer_name: z.string().trim().regex(TEXT_SEARCH_REGEX).optional(),
-  yeast: z.string().trim().regex(TEXT_SEARCH_REGEX).optional(),
-  brewed_before: z
-    .string()
+  page: z.coerce
+    .number(responses.datatype.number)
+    .int(responses.datatype.int)
+    .min(1, responses.conditions.min(1))
+    .optional(),
+  per_page: z.coerce
+    .number(responses.datatype.number)
+    .int(responses.datatype.int)
+    .min(1, responses.conditions.min(1))
+    .max(80, responses.conditions.max(80))
+    .optional(),
+  abv_gt: z.coerce.number(responses.datatype.number).optional(),
+  abv_lt: z.coerce.number(responses.datatype.number).optional(),
+  ibu_gt: z.coerce.number(responses.datatype.number).optional(),
+  ibu_lt: z.coerce.number(responses.datatype.number).optional(),
+  ebc_gt: z.coerce.number(responses.datatype.number).optional(),
+  ebc_lt: z.coerce.number(responses.datatype.number).optional(),
+  beer_name: z
+    .string(responses.datatype.string)
     .trim()
-    .regex(BEERS_BREWED_REGEX)
-    .refine(checkBrewedDateFormat)
+    .regex(TEXT_SEARCH_REGEX, responses.custom.textSearch)
+    .optional(),
+  yeast: z
+    .string(responses.datatype.string)
+    .trim()
+    .regex(TEXT_SEARCH_REGEX, responses.custom.textSearch)
+    .optional(),
+  brewed_before: z
+    .string(responses.datatype.string)
+    .trim()
+    .regex(BEERS_BREWED_REGEX, responses.custom.brewedDate)
+    .refine(checkBrewedDateRange, responses.custom.brewedLimit)
     .optional(),
   brewed_after: z
-    .string()
+    .string(responses.datatype.string)
     .trim()
-    .regex(BEERS_BREWED_REGEX)
-    .refine(checkBrewedDateFormat)
+    .regex(BEERS_BREWED_REGEX, responses.custom.brewedDate)
+    .refine(checkBrewedDateRange, responses.custom.brewedLimit)
     .optional(),
-  hops: z.string().trim().regex(TEXT_SEARCH_REGEX).optional(),
-  malt: z.string().trim().regex(TEXT_SEARCH_REGEX).optional(),
-  food: z.string().trim().regex(TEXT_SEARCH_REGEX).optional(),
-  ids: z
-    .string()
+  hops: z
+    .string(responses.datatype.string)
     .trim()
-    .regex(BEERS_IDS_REGEX)
-    .refine(checkIfAnyIDIsOutOfLimit, IDLimitError)
+    .regex(TEXT_SEARCH_REGEX, responses.custom.textSearch)
+    .optional(),
+  malt: z
+    .string(responses.datatype.string)
+    .trim()
+    .regex(TEXT_SEARCH_REGEX, responses.custom.textSearch)
+    .optional(),
+  food: z
+    .string(responses.datatype.string)
+    .trim()
+    .regex(TEXT_SEARCH_REGEX, responses.custom.textSearch)
+    .optional(),
+  ids: z
+    .string(responses.datatype.string)
+    .trim()
+    .regex(BEERS_IDS_REGEX, responses.custom.idsFormat)
+    .refine(checkIfAnyIDIsOutOfLimit, responses.custom.idLimit)
     .optional(),
 });
