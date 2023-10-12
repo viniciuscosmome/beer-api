5db8fc01059329a12292d0c3155d17fa61b71f8c
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-10-12 09:21:42 -0300

    chore: find beers with advanced filters

--- src/modules/beers/beers.service.ts
+++ src/modules/beers/beers.service.ts
@@ -2,6 +2,19 @@ import { PunkApi } from '../../lib/axios';
 import { handleErrors } from '../../globals/errors';
 
 export const beersService = {
+  async findBeersWithAdvancedFilters(filters: object) {
+    const search = await PunkApi.get('', { params: filters })
+      .then((response) => {
+        return response.data;
+      })
+      .catch((error) => {
+        handleErrors(error);
+        return [];
+      });
+
+    return search;
+  },
+
   async findBeersWithBasicFilters(filter: string) {
     const beer = await PunkApi.get(filter)
       .then((response) => {
