58ef6a777d0bc1793d0f9118f8f586da5e4afd62
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-10-12 08:34:13 -0300

    chore: adds advanced filters

--- src/@types/api/beers.d.ts
+++ src/@types/api/beers.d.ts
@@ -1,4 +1,17 @@
 declare type iAdvancedFilters = {
   page?: number | undefined;
-  per_page?: number | undefined;
+  abv_gt?: number | undefined;
+  abv_lt?: number | undefined;
+  ibu_gt?: number | undefined;
+  ibu_lt?: number | undefined;
+  ebc_gt?: number | undefined;
+  ebc_lt?: number | undefined;
+  beer_name?: string | undefined;
+  yeast?: string | undefined;
+  brewed_before?: string | undefined;
+  brewed_after?: string | undefined;
+  hops?: string | undefined;
+  malt?: string | undefined;
+  food?: string | undefined;
+  ids?: string | undefined;
 };
