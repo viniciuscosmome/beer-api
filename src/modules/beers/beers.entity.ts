a8b874431eacbdbcf819fed0c18509c9c9d8ee69
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-10-12 05:10:34 -0300

    chore: describe custom response

--- src/modules/beers/beers.entity.ts
+++ src/modules/beers/beers.entity.ts
@@ -3,5 +3,7 @@ import { BASIC_SEARCH_REGEX } from '../../globals/constants';
 import { limitSearchID } from '../../globals/utilities';
 
 export const BasicSearchEntity = z.object({
-  filter: z.string().regex(BASIC_SEARCH_REGEX).refine(limitSearchID),
+  filter: z.string().regex(BASIC_SEARCH_REGEX).refine(limitSearchID, {
+    message: 'The ID must be between 1 and 325',
+  }),
 });
