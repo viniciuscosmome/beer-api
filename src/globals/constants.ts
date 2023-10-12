6fd48a94d89118f109336817eaf97c370fefe266
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-10-12 08:59:35 -0300

    feat: creates filters regex

--- src/globals/constants.ts
+++ src/globals/constants.ts
@@ -8,6 +8,9 @@ export const SECRET_SESS = process.env.SECRET_SESS as string;
 
 export const PASSWORD_REGEX = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W).*$/;
 export const BASIC_SEARCH_REGEX = /^(random|\d+)$/;
+export const TEXT_SEARCH_REGEX = /^[a-zÀ-ÿ][a-zÀ-ÿ_]+[a-zÀ-ÿ]$/i;
+export const BEERS_IDS_REGEX = /^[0-9][0-9|]+[0-9]$/;
+export const BEERS_BREWED_REGEX = /^[0-9]{1,2}-[0-9]{1,4}$/;
 
 export const APP_TITLE = 'Beer API';
 export const APP_LICENSE = 'MIT License';
