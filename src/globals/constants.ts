0777c8ab33fdc249d8679270faea1aad1474b119
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-10-13 15:05:33 -0300

    docs: changes port

--- src/globals/constants.ts
+++ src/globals/constants.ts
@@ -1,4 +1,4 @@
-export const PORT = process.env.PORT || 5500;
+export const PORT = process.env.PORT || 8080;
 export const IS_PRODUCTION_ENV = process.env.NODE_ENV === 'production';
 export const IS_HOMOLOGATION_ENV = process.env.NODE_ENV === 'homologation';
 export const IS_DEVELOPMENT_ENV = !IS_PRODUCTION_ENV && !IS_HOMOLOGATION_ENV;
